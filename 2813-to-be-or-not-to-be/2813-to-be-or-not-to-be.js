38c462b8140961994306c598706b35bec7194ad2
Mohsin Behzad <128903293+mohsinbehzad@users.noreply.github.com>
2025-05-08 22:29:19 +0500

    Time: 50 ms (12.57%), Space: 53.7 MB (27.04%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe: function(value){
+            if(value === val){
+                return true;
+            }
+            else{
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function(value){
+            if(value !== val){
+                return true;
+            }
+            else{
+                throw new Error("Equal")
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
